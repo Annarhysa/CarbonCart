e16b8d7d21f5f13c22bf3adcea696b1fe7e6fab3
Prithvi2310 <shahprithvi1505@gmail.com>
2024-07-27 14:31:59 +0530

    read the txt file into the python file

--- models/check_item.py
+++ models/check_item.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv('../data/items_cp_stats.csv')
+data = pd.read_csv('data/items_cp_stats.csv')
+
+with open('data/extracted_text.txt','r') as f:
+    myList = f.readlines()
+    myList = map(lambda x: x.replace('\n',''),myList)
 
